2a04eef113c3cb21a39c17fbc85588716d30d732
Sebastian Georgi <sebastian@georgi.io>
2021-09-10 16:51:34 +0200

    Demodularised tests to adhere to common Rust standards

--- ryw-backend/src/main.rs
+++ ryw-backend/src/main.rs
@@ -1,8 +1,6 @@
 #[macro_use]
 extern crate rocket;
 
-mod tests;
-
 use rocket::serde::json::Json;
 use serde::{Serialize, Deserialize};
 
@@ -26,4 +24,35 @@ fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rocket::http::Status;
 
+    mod test_helper {
+        use crate::rocket;
+        use rocket::local::blocking::Client;
+
+        pub fn client() -> Client {
+            Client::tracked(rocket()).expect("valid rocket instance")
+        }
+    }
+
+    #[test]
+    fn test_index() {
+        let client = test_helper::client();
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+
+    #[test]
+    fn test_index_json() {
+        let msg =  Test {
+            title: "Title".to_string(),
+            body: "This is the body".to_string()
+        };
+        let client = test_helper::client();
+        let response = client.get("/").dispatch();
+        assert_eq!(response.into_json(), Some(msg));
+    }
+}
