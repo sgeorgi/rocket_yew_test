235046de09dd79bf40433d369c601f70b745314c
Sebastian Georgi <sebastian@georgi.io>
2021-09-10 13:27:17 +0200

    Test for Json output added

--- ryw-backend/src/main.rs
+++ ryw-backend/src/main.rs
@@ -2,9 +2,9 @@
 extern crate rocket;
 
 use rocket::serde::json::Json;
-use serde::{Serialize};
+use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Test {
     title: String,
     body: String
@@ -22,4 +22,25 @@ fn index() -> Json<Test> {
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+    use crate::Test;
+
+    #[test]
+    fn test_index() {
+        let msg = Test {
+            title: "Title".to_string(),
+            body: "This is the body".to_string()
+        };
+
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_json(), Some(msg));
+    }
 }
