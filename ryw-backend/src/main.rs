308c368d3466d6015406528d8af750c0b7bf4f1e
Sebastian Georgi <sebastian@georgi.io>
2021-09-10 12:25:33 +0200

    Initial JSON backend with Rocket.rs

--- ryw-backend/src/main.rs
+++ ryw-backend/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::json::Json;
+use serde::{Serialize};
+
+#[derive(Debug, Serialize)]
+struct Test {
+    title: String,
+    body: String
+}
+
+#[get("/")]
+fn index() -> Json<Test> {
+    let msg = Test {
+        title: "Title".to_string(),
+        body: "This is the body".to_string()
+    };
+    Json(msg)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
